336f7bd426d601f42905381dafbed68f5377f05b
NagarajMuthuvaradhan <nagaraj.muthuvaradhan@vgw.co>
2014-02-07 15:05:34 +0530

    Dev commit 1

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ header("Expires: Wed, 12 Dec 1990 12:12:12 GMT");
 //echo $_SESSION['SITE_LOC'];
 //Cookie Checking
 $extGame = 'false';
-$cookId = 'ccrm_v24New file';
+$cookId = 'ccrm_v24Check';
 if(isset($_COOKIE[$cookId]))
  {  	
  	//echo "Welcome back!";
